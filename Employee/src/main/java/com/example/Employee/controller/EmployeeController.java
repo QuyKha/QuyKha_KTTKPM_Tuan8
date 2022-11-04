6da23ed55a9e5a278c810b96efbecb00d644d6fb
QKDev <nguyenquykha123@gmail.com>
2022-11-04 12:07:35 +0700

    circuitbreaker

--- Employee/src/main/java/com/example/Employee/controller/EmployeeController.java
+++ Employee/src/main/java/com/example/Employee/controller/EmployeeController.java
@@ -4,12 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.stereotype.Controller;
 
 @RestController
 @RequestMapping(value = "/employee" )
 public class EmployeeController {
-    @GetMapping
-    public String getOrder() {
-        return "Hello from Employee Service";
+	  @GetMapping
+	    @CircuitBreaker(name = "inventory",fallbackMethod = "fallbackMethod")
+	    public String getOrder() {
+	        return "Hello from Order Service";
+	    }
+
+	    public String fallbackMethod(RuntimeException runtimeException) {
+	        return "Something went wrong, please request order a next time";
     }
 }
